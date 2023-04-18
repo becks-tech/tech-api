b8af0ecdd8c1ebe8bb1b52c05055c9e1410657f5
Rebecca <techybecky21@gmail.com>
2023-04-18 18:53:01 +0100

    add random number generator

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let url = 'https://techy-api.vercel.app/api/text'
-
+const num = [0,1,2,3,4,5,6,7,8,9]
+const adviceNum = document.querySelector("")
 document.querySelector('button').addEventListener('click', () =>{
     console.log('About to fetch')
     fetch(url).then((response) => {
@@ -10,4 +11,16 @@ document.querySelector('button').addEventListener('click', () =>{
         console.log(data)
         document.querySelector('.par').innerHTML = data;
     })
+    console.log('Getting random number now...')
+    randNum = ''
+    for (let i=0;i<4; i++){
+        randNum += num[getRandomNumber()];
+    }
+    adviceNum.textContent
+}
 })
+function getRandomNumber(){
+    return Math.floor(Math.random() * 4000);
+}
+
+
